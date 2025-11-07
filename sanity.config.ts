efc0a263fbf3d5501a36340d0efcd3701f785538
havardhuns <havardhuns@gmail.com>
2025-11-07 11:04:09 +0100

    Add deploy button to sanity

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,28 +1,41 @@
-'use client'
+"use client";
 
 /**
  * This configuration is used to for the Sanity Studio that’s mounted on the `/app/admin/[[...tool]]/page.tsx` route
  */
 
-import {visionTool} from '@sanity/vision'
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
+import { visionTool } from "@sanity/vision";
+import { defineConfig } from "sanity";
+import { structureTool } from "sanity/structure";
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import {apiVersion, dataset, projectId} from './src/sanity/env'
-import {schema} from './src/sanity/schemaTypes'
-import {structure} from './src/sanity/structure'
+import { apiVersion, dataset, projectId } from "./src/sanity/env";
+import { schema } from "./src/sanity/schemaTypes";
+import { structure } from "./src/sanity/structure";
+
+import { vercelDeployTool } from "sanity-plugin-vercel-deploy";
 
 export default defineConfig({
-  basePath: '/admin',
+  basePath: "/admin",
   projectId,
   dataset,
   // Add and edit the content schema in the './sanity/schemaTypes' folder
   schema,
   plugins: [
-    structureTool({structure}),
+    structureTool({ structure }),
     // Vision is for querying with GROQ from inside the Studio
     // https://www.sanity.io/docs/the-vision-plugin
-    visionTool({defaultApiVersion: apiVersion}),
+    visionTool({ defaultApiVersion: apiVersion }),
+    vercelDeployTool({
+      // Optional preconfigured projects
+      projects: [
+        {
+          name: process.env.NEXT_PUBLIC_VERCEL_ENV!,
+          projectId: process.env.NEXT_PUBLIC_VERCEL_PROJECT_ID,
+          teamId: process.env.NEXT_PUBLIC_VERCEL_TEAM_ID,
+          url: "https://api.vercel.com/v1/integrations/deploy/prj_UivqS5XXAbsZtNkpVuxnPhGaHbA2/j2LQ6TWpzn",
+        },
+      ],
+    }),
   ],
-})
+});
